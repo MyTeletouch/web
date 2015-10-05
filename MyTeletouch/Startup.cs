fce8b1716eebf1c6a8c410998b692e6206991501
Dimitar Danailov <dimityr.danailov@gmail.com>
2015-10-05 14:23:43 +0000

    Add Project to repository.

---
+++ MyTeletouch/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(MyTeletouch.Startup))]
+namespace MyTeletouch
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
