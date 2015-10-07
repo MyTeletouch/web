51b75a592325068b07d9563064e0f33bd3839954
Dimitar Danailov <dimityr.danailov@gmail.com>
2015-10-07 06:34:26 +0000

    Start implementation of paypal.

---
+++ MyTeletouch.Entities/Order.cs
@@ -0,0 +1,40 @@
+﻿using ClientConfigurations;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyTeletouch.Entities
+{
+    public class Order
+    {
+        [Key]
+        public int OrderId { get; set; }
+
+        // Foreign Key
+        [Required]
+        [MaxLength(128, ErrorMessage = "Maximum length is {0} characters.")]
+        public string UserID { get; set; }
+
+        [DataType(DataType.Date)]
+        [DisplayFormat(DataFormatString = DateSettings.StandartDateFormat, ApplyFormatInEditMode = true)]
+        public DateTime OrderDate { get; set; }
+
+        [ForeignKey("UserID")]
+        public virtual ApplicationUser User { get; set; }
+
+        [ScaffoldColumn(false)]
+        public decimal Total { get; set; }
+
+        [ScaffoldColumn(false)]
+        public string PaymentTransactionId { get; set; }
+
+        [ScaffoldColumn(false)]
+        public bool HasBeenShipped { get; set; }
+
+        public List<OrderDetail> OrderDetails { get; set; }
+    }
+}
