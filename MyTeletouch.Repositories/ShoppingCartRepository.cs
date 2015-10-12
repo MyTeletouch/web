6a7dfb67f76a0a14a96bbaa9ee8e33b767ad123c
Dimitar Danailov <dimityr.danailov@gmail.com>
2015-10-12 20:31:35 +0000

    Implemented IShoppingCartRepository. Next task we will implementation of ShoppingCartRepository.

--- MyTeletouch.Repositories/ShoppingCartRepository.cs
+++ MyTeletouch.Repositories/ShoppingCartRepository.cs
@@ -5,13 +5,19 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using MyTeletouch.Repositories.Intefraces;
 
 namespace MyTeletouch.Repositories
 {
-    public class ShoppingCartRepository : Repository<CartItem>
+    public class ShoppingCartRepository : Repository<CartItem>, IShoppingCartRepository
     {
+        public string ShoppingCartId { get; set; }
+
+        public const string CartSessionKey = "CartId";
+
         public ShoppingCartRepository() : base(new ProductDbContext())
         {
+            
         }
     }
 }
