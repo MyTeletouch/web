2dd06de15acf2784a75c7f258d3be057508f6085
Dimitar Danailov <dimityr.danailov@gmail.com>
2016-03-25 10:00:01 +0000

    ViewHelpers will be used for Custom HTML Helpers

---
+++ ViewHelpers/LinkHelper.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ViewHelpers
+{
+    public class LinkHelper
+    {
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="type"></param>
+        /// <param name="hrefLocation"></param>
+        /// <returns></returns>
+        public static string Link(string type, string hrefLocation)
+        {
+            // string urlContent = Url.Content
+            string link = String.Format("<link rel='{0}' href='{1}'/>", type, hrefLocation);
+
+            return link;
+        }
+    }
+}
