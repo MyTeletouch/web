7bfd54e84802408e3e707b830969b8a48ecee096
Dimitar Danailov <dimityr.danailov@gmail.com>
2016-05-07 10:32:03 +0000

    Added a custom helper for loading a link rel resources. MvcHtmlString is using for to avoid html encoding.

--- ViewHelpers/LinkHelper.cs
+++ ViewHelpers/LinkHelper.cs
@@ -1,25 +1,34 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.Web.Mvc;
 
 namespace ViewHelpers
 {
     public class LinkHelper
     {
         /// <summary>
-        /// 
+        /// Generate link rel HTML.
         /// </summary>
+        /// <see cref="https://stackoverflow.com/questions/2293357/what-is-an-mvchtmlstring-and-when-should-i-use-it#_=_"/>
         /// <param name="type"></param>
         /// <param name="hrefLocation"></param>
         /// <returns></returns>
-        public static string Link(string type, string hrefLocation)
+        public static MvcHtmlString Link(string type, string hrefLocation)
         {
             // string urlContent = Url.Content
-            string link = String.Format("<link rel='{0}' href='{1}'/>", type, hrefLocation);
+            string link = String.Format("<link rel=\"{0}\" href=\"{1}\" />", type, hrefLocation);
+            var mvcOutput = new MvcHtmlString(link);
 
-            return link;
+            return mvcOutput;
+        }
+
+        /// <summary>
+        /// Generate link rel HTML import.
+        /// </summary>
+        /// <param name="hrefLocation"></param>
+        /// <returns></returns>
+        public static MvcHtmlString LinkImport(string hrefLocation)
+        {
+            return Link("import", hrefLocation);
         }
     }
 }
